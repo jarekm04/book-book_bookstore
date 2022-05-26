2ff34de8fe80fec7f6c5ecb91e0c9bfd7bbf5437
Jarek <jarek_m94@wp.pl>
2022-05-26 20:23:56 +0200

    books styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,21 +4,23 @@ import {AiOutlineShoppingCart} from "react-icons/ai";
 const Header = () => {
     return (
         <header className="header">
-            <Link to="/" className="header__logo">
-                <p className="logo__textTop">
-                    Book-Book
-                </p>
-                <p className="logo__textBottom">
-                    Who's there?
-                </p>
-            </Link>
-            <div className="header__purchase">
-                <p className="purchase__price">
-                    150zł
-                </p>
-                <Link to="/shoppingCart">
-                    <AiOutlineShoppingCart className="purchase__icon" />
+            <div className="container">
+                <Link to="/" className="header__logo">
+                    <p className="logo__textTop">
+                        Book-Book
+                    </p>
+                    <p className="logo__textBottom">
+                        Who's there?
+                    </p>
                 </Link>
+                <div className="header__purchase">
+                    <p className="purchase__price">
+                        150zł
+                    </p>
+                    <Link to="/shoppingCart">
+                        <AiOutlineShoppingCart className="purchase__icon"/>
+                    </Link>
+                </div>
             </div>
         </header>
     );
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,14 +3,58 @@ import boook from "../assets/458.jpg";
 const Home = () => {
     return (
         <section className="home">
-            <article className="home__book">
-                <img src={boook} className="book__img" alt="bookPhoto"/>
-                <h3 className="book__title">Matematyka 1. Podręcznik. Zakres podstawowy</h3>
-                <p className="book__author">M. Karpiński, M. Dobrowolska, M. Braun, J. Lech</p>
-                <p className="book__pages">500 stron</p>
-                <p className="book__price">466zł</p>
-                <button className="book__add">Dodaj do koszyka</button>
-            </article>
+            <div className="container">
+                <article className="home__book">
+                    <img src={boook} className="book__img" alt="bookPhoto"/>
+                    <div className="book__textBox">
+                        <h3 className="book__title">Matematyka 1. Podręcznik. Zakres podstawowy</h3>
+                        <p className="book__author">M. Karpiński, M. Dobrowolska, M. Braun, J. Lech</p>
+                        <p className="book__pages">500 stron</p>
+                        <p className="book__price">Cena: 466zł</p>
+                        <button className="book__add">Dodaj do koszyka</button>
+                    </div>
+                </article>
+                <article className="home__book">
+                    <img src={boook} className="book__img" alt="bookPhoto"/>
+                    <div className="book__textBox">
+                        <h3 className="book__title">Matematyka 1. Podręcznik. Zakres podstawowy</h3>
+                        <p className="book__author">M. Karpiński, M. Dobrowolska, M. Braun, J. Lech</p>
+                        <p className="book__pages">500 stron</p>
+                        <p className="book__price">Cena: 466zł</p>
+                        <button className="book__add">Dodaj do koszyka</button>
+                    </div>
+                </article>
+                <article className="home__book">
+                    <img src={boook} className="book__img" alt="bookPhoto"/>
+                    <div className="book__textBox">
+                        <h3 className="book__title">Matematyka 1. Podręcznik. Zakres podstawowy</h3>
+                        <p className="book__author">M. Karpiński, M. Dobrowolska, M. Braun, J. Lech</p>
+                        <p className="book__pages">500 stron</p>
+                        <p className="book__price">Cena: 466zł</p>
+                        <button className="book__add">Dodaj do koszyka</button>
+                    </div>
+                </article>
+                <article className="home__book">
+                    <img src={boook} className="book__img" alt="bookPhoto"/>
+                    <div className="book__textBox">
+                        <h3 className="book__title">Matematyka 1. Podręcznik. Zakres podstawowy</h3>
+                        <p className="book__author">M. Karpiński, M. Dobrowolska, M. Braun, J. Lech</p>
+                        <p className="book__pages">500 stron</p>
+                        <p className="book__price">Cena: 466zł</p>
+                        <button className="book__add">Dodaj do koszyka</button>
+                    </div>
+                </article>
+                <article className="home__book">
+                    <img src={boook} className="book__img" alt="bookPhoto"/>
+                    <div className="book__textBox">
+                        <h3 className="book__title">Matematyka 1. Podręcznik. Zakres podstawowy</h3>
+                        <p className="book__author">M. Karpiński, M. Dobrowolska, M. Braun, J. Lech</p>
+                        <p className="book__pages">500 stron</p>
+                        <p className="book__price">Cena: 466zł</p>
+                        <button className="book__add">Dodaj do koszyka</button>
+                    </div>
+                </article>
+            </div>
         </section>
     );
 };
